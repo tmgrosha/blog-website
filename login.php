cf6b12e7f4e2f1452d4d856f287bd1160e260d9c
Rk tamang <tmgroshan2004@gmail.com>
2024-05-14 17:42:34 +0545

    updatlogin.php

--- login.php
+++ login.php
@@ -18,3 +18,18 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     }
 }
 ?>
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    fsdfasdf
+    
+</body>
+</html>
